601e741e3d898ce3ff4852f1955adbcbb5eb17ef
Ryan Fritz <ryanfritzc@gmail.com>
2024-09-12 11:40:24 -0400

    fixed the swiper slides on the home page and made new components for easy of update and structure

---
+++ app/Components/TeamSwiperPortraits.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+interface TeamSwiperProps {
+  image: string;
+  name: string;
+}
+
+const TeamSwiperPortraits: React.FC<TeamSwiperProps> = ({image, name})=> {
+  return (
+      <div className="flex flex-col items-center w-[calc(50%-0.5rem)] md:w-[calc(33.33%-0.5rem)]">
+        <img src={image} className="max-w-[150px] max-h-[125px] md:max-w-[600px] md:max-h-[305px]"/>
+        <h2>{name}</h2>
+      </div>
+  );
+}
+export default TeamSwiperPortraits;
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,86 @@
 "use client";
-import Image from "../public/homepage/100Thieves-CodRoster.png";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
-import "./CssModules-Css/home.css"
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import "./CssModules-Css/home.css";
+import TeamSwiperPortraits from "./Components/TeamSwiperPortraits";
 
 export default function Home() {
   return (
-      <Swiper
-        pagination={true}
-        modules={[Pagination]}
-        >
-        <SwiperSlide>
-          <img src="/homepage/100Thieves-CodRoster.png" alt="LA Thieves" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/homepage/100Thieves-lolRoster.png" alt="LA Thieves" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/homepage/100Thieves-valRoster.webp" alt="LA Thieves" />
-        </SwiperSlide>
-      </Swiper>
+    <Swiper pagination={true} modules={[Pagination]}>
+      <SwiperSlide>
+        <div className="flex flex-wrap ">
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/ghosty.png"
+            name="GHOSTY"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/joedeceives.png"
+            name="JOEDECEIVES"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/laPlaceholder.png"
+            name="NASTIE"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/laPlaceholder.png"
+            name="KREMP"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="flex flex-wrap ">
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/quid.png"
+            name="quid"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/river.png"
+            name="river"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/sniper.png"
+            name="sniper"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/meech.png"
+            name="meech"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/eyla.png"
+            name="eyla"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/tomo.png"
+            name="tomo"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="flex flex-wrap ">
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/asuna.png"
+            name="asuna"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/cryo.png"
+            name="cryo"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/boostio.png"
+            name="boostio"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/bang.png"
+            name="bang"
+          />
+          <TeamSwiperPortraits
+            image="/homepage/team-portraits/eeiu.png"
+            name="eeiu"
+          />
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
